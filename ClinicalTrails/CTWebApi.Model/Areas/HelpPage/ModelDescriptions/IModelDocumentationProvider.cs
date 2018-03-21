88a74027fca591b4ef35e12cf7700e067a5d6be5
Gyandeep Singh <gyandeep.singh@external.atlascopco.com>
2018-03-21 20:17:03 +0530

    Added New Projects and Mapping

---
+++ ClinicalTrails/CTWebApi.Model/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace CTWebApi.Model.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ ClinicalTrails/CTWebApi.Model/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CTWebApi.Model.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
